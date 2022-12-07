430541dffc3e1a4a6752f4f5efd3e28812e9eb72
alirezarahatloo <rahatlooalireza@gmail.com>
2022-12-07 14:27:27 +0330

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,20 @@ int main(int argc, char **argv)
   if(stat(argv[1],&fileStat) < 0)    
     return 1;
   printf("Information for %s\n",argv[1]);
+  printf("\nFile Permissions User\n");
+  printf((fileStat.st_mode & S_IRUSR)? "r":"-");
+  printf((fileStat.st_mode & S_IWUSR)? "w":"-");
+  printf((fileStat.st_mode & S_IXUSR)? "x":"-");
+  printf("\n");
+  printf("\nFile Permissions Group\n");
+  printf((fileStat.st_mode & S_IRGRP)? "r":"-");
+  printf((fileStat.st_mode & S_IWGRP)? "w":"-");
+  printf((fileStat.st_mode & S_IXGRP)? "x":"-");
+  printf("\n");
+  printf("\nFile Permissions Other\n");
+  printf((fileStat.st_mode & S_IROTH)? "r":"-");
+  printf((fileStat.st_mode & S_IWOTH)? "w":"-");
+  printf((fileStat.st_mode & S_IXOTH)? "x":"-");
 
 
 }
